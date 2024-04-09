7ca06ad07ef9cc11c598539979a3e8e16ce66cd4
deajan <ozy@netpower.fr>
2024-04-09 14:23:02 +0200

    Don't bother check for MSI file

--- windows_exporter_installer.cmd
+++ windows_exporter_installer.cmd
@@ -4,8 +4,8 @@ set curdir=%~dp0
 set curdir=%curdir:~0,-1%
 set curdrive=%~d0
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`dir windows_exporter*.msi /b/s`) DO (
-SET msi_file=%%F
-)
+::FOR /F "tokens=* USEBACKQ" %%F IN (`dir windows_exporter*.msi /b/s`) DO (
+::SET msi_file=%%F
+::)
 
 powershell -ExecutionPolicy Bypass -NonInteractive -NoLogo -File %curdir%\windows_exporter_installer.ps1
