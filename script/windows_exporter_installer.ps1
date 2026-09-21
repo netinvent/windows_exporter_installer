64ce5ed0c402788dae4ed8eed1ca2e5bc20f99bf
Orsiris de Jong <ozy@netpower.fr>
2026-09-21 10:21:18 +0200

    Bump script version

--- script/windows_exporter_installer.ps1
+++ script/windows_exporter_installer.ps1
@@ -25,7 +25,7 @@
 
 
 # Script version needs to be a float so we can easily compare with previous setups
-$SCRIPT_VERSION = 1.2
+$SCRIPT_VERSION = 1.4
 
 $git_org = "prometheus-community"
 $git_repo = "windows_exporter"
